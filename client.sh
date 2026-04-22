f8b324121dce1f6e052c35478c5eafb83a40fbd7
lemonlover31415 <xaga@smmk12.org>
2026-04-22 16:56:53 -0700

    Create client.sh

---
+++ client.sh
@@ -0,0 +1,28 @@
+export version="$(curl -sL https://github.com/zardoy/minecraft-web-client/releases | grep -oP "v[0-9]+\.[0-9]+\.[0-9]+" | sort -V | tail -n 1)"
+export node_version="$(nvm ls-remote | tr -s ' ' | sed 's|^ ||g' | grep -i latest | grep -i lts | cut -d ' ' -f1 | tr -d 'v' | sort -V | tail -n 1)"
+export url="https://github.com/zardoy/minecraft-web-client/releases/download/${version}/self-host.zip"
+export repo="/minecraft-on-codespaces"
+export codespace="$(echo $(gh codespace list | grep -i ${repo}) | tr -s ' ' | cut -d ' ' -f1)"
+export proxy="$(echo $(gh codespace ports -c $codespace | $codespace) | tr -s ' ' | cut -d ' ' -f3,4 | grep -P '.*-6767\.app')"
+
+sudo apt install wget -y
+
+nvm install $node_version
+nvm use $node_version
+nvm alias default $node_version
+
+wget $url
+unzip self-host.zip
+rm -r dist
+cp server.js proxy.js
+mv server.js client.js
+sed -i 's|8080|6767|g' proxy.js
+
+cat << EOF >> ~/.profile
+node proxy.js &
+export proxy="$(echo $(gh codespace ports -c $codespace | $codespace) | tr -s ' ' | cut -d ' ' -f3,4 | grep -P '.*-6767\.app')"
+sed -i "s|defaultProxy.*|defaultProxy: \"${proxy}:443\"|g" client.js
+node client.js &
+EOF
+
+source ~/.profile
